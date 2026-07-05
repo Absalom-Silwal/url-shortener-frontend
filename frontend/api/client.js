17fc6d42207bb676b6b19f08b0aeff4b5c517e10
absalom <absalomsilwal93@gmail.com>
2026-07-05 17:43:14 +1000

    fixed:clientjs serverendpoint fixed

--- frontend/api/client.js
+++ frontend/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const client = axios.create({
   //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
-  baseURL: 'https://app-shortly.onrender.com' || 'http://localhost:8080'
+  baseURL: 'https://app-shortly.onrender.com/api/v1/' || 'http://localhost:5000/api/v1'
 });
 
 export default client;
